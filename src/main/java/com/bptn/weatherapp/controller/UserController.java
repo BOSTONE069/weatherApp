e2fa25c81ae907280b0c1eded833f5f63ffa5369
BOSTONE069 <bostoneochieng69@gmail.com>
2023-10-19 12:48:11 +0300

    Updated the user controller with the reset email passord method

--- src/main/java/com/bptn/weatherapp/controller/UserController.java
+++ src/main/java/com/bptn/weatherapp/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +58,14 @@ public class UserController {
 		logger.debug("User Authenticated, username: {}", user.getUsername());
 			
 		return new ResponseEntity<>(user, jwtHeader, OK);
+	}
+	
+	@GetMapping("/reset/{emailId}")
+	public void sendResetPasswordEmail(@PathVariable String emailId) {
+			
+			logger.debug("Sending Reset Password Email, emailId: {}", emailId);
+			
+			this.userService.sendResetPasswordEmail(emailId);
 	}
     
 }
